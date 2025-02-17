e4f00a2eaf705d5f97ff97450e5237aaec4e9fad
Abdullah Siddiqi <abdullahhsiddiqi@gmail.com>
2025-02-17 19:56:22 +0500

    files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+//import { useState } from 'react';//leftover from default, delete maybe?
+import './App.css';
+import "./index.css";
+import { LoadingScreen } from './components/loadingScreen';
+import { Navbar} from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+
+
+function App() {
+  //this section is to check if our loading screem schould be shown or not.
+  const [isLoaded, setIsLoaded] = useState(false); //setIsLoaded is a function 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+return (
+   <> 
+   {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+    <div
+     className={`min-h-screen transition-opacity duration-700 ${
+      isLoaded ? "opacity-100" : "opacity-0"}
+       bg-black text-gray-500`}
+       >
+
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> 
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> 
+
+       </div>
+
+   
+
+   </> 
+      );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' 
+
+// https://vite.dev/config/
+export default defineConfig({     
+  plugins: [react(), tailwindcss()],  //if tailwing is not added here AND imported at the top none of the css styling will show up.       
+})
