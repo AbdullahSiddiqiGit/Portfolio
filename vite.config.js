f12e11869ac175acb954f5b13e7f9ca150aa18ec
Abdullah Siddiqi <abdullahhsiddiqi@gmail.com>
2025-02-20 17:32:14 +0500

    directories added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({     
-  plugins: [react(), tailwindcss()],  //if tailwing is not added here AND imported at the top none of the css styling will show up.       
+  plugins: [react(), tailwindcss()], //if tailwing is not added here AND imported at the top none of the css styling will show up.    
+  base: "Portfolio", //this is the repo name on github for gh pages
 })
